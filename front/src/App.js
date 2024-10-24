1cdfa26678e11787911bfa500de2c3b6723bf15e
Tanzil <tanzil@MacBook-Air-2.local>
2024-10-25 01:47:48 +0530

    changes

--- front/src/App.js
+++ front/src/App.js
@@ -22,7 +22,7 @@ function App() {
     formData.append('file', selectedFile);
 
     try {
-      const response = await axios.post('http://localhost:5000/predict', formData, {
+      const response = await axios.post('https://ml-project-amber.vercel.app//predict', formData, {
         headers: {
           'Content-Type': 'multipart/form-data',
         },
