a436de58705efdd92ed97d9d37ae8fcc9860419f
Tanzil <tanzil@MacBook-Air-2.local>
2024-10-25 02:02:32 +0530

    changes

--- front/src/App.js
+++ front/src/App.js
@@ -22,7 +22,7 @@ function App() {
     formData.append('file', selectedFile);
 
     try {
-      const response = await axios.post('https://mlproject-nqot.onrender.com/predict', formData, {
+      const response = await axios.post('https://mlproject-578n.onrender.com/predict', formData, {
         headers: {
           'Content-Type': 'multipart/form-data',
         },
