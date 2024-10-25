c3088e665e70976c28d086c5297ae9ec8b32fb16
Tanzil <tanzil@MacBook-Air-2.local>
2024-10-25 15:12:15 +0530

    chenges

--- front/src/App.js
+++ front/src/App.js
@@ -27,7 +27,7 @@ function App() {
     setLoading(true); // Start loading state
 
     try {
-      const response = await axios.post('https://mlproject-api.onrender.com//predict', formData, {
+      const response = await axios.post('https://mlproject-api.onrender.com/predict', formData, {
         headers: {
           'Content-Type': 'multipart/form-data',
         },
